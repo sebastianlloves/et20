6935193e266b9d28811c87d63f3a46235af1e064
sebastianlloves <sebastianlloves@gmail.com>
2024-08-20 16:10:12 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body
         className={cn(
           /* inter.className,  */ openSans.className,
