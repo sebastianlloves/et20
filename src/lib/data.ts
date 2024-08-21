3cfb18d7c853f39169e47469b082fc1f968c4143
sebastianlloves <sebastianlloves@gmail.com>
2024-08-21 00:26:30 -0300

    .

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,7 +2,7 @@ import { DB } from './constants'
 import { formatStudentsResponse } from './utils'
 
 export async function fetchStudentsData(anio: string) {
-  const response = await fetch(DB[anio], {cache: 'force-cache'})
+  const response = await fetch(DB[anio])
   const textData = await response.text()
   // await new Promise((resolve) => setTimeout(resolve, 2000))
 
