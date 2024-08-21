9282156d3d45560c20aa0adb412398ad16ac6339
sebastianlloves <sebastianlloves@gmail.com>
2024-08-20 21:00:25 -0300

    .

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,7 +4,7 @@ import { formatStudentsResponse } from './utils'
 export async function fetchStudentsData(anio: string) {
   const response = await fetch(DB[anio])
   const textData = await response.text()
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  // await new Promise((resolve) => setTimeout(resolve, 2000))
 
   return formatStudentsResponse(textData)
 }
