43f1b4f648173fee3454ae2d020251e786a52e45
sebastianlloves <sebastianlloves@gmail.com>
2024-10-09 21:57:45 -0300

    .

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,11 +1,12 @@
 import { revalidateTag } from 'next/cache'
-import { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const tags = request.nextUrl.searchParams.get('tags')?.split('_')
-  tags?.forEach((tag) => revalidateTag(tag))
-  console.log(tags)
-
-  return Response.json({ revalidatedTags: tags })
+  const tag = request.nextUrl.searchParams.get('tag')
+  if(tag) {
+    revalidateTag(tag)
+    return Response.json({ revalidated: true, now: Date.now() })
+  }
+  return Response.json({ revalidated: false, now: Date.now() })
 }
 
