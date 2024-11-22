e9409100cd4a2b10d417a0b27f3cf639e038da3b
sebastianlloves <sebastianlloves@gmail.com>
2024-11-22 16:29:52 -0300

    .

--- src/app/analisis-academico/page.tsx
+++ src/app/analisis-academico/page.tsx
@@ -1,27 +1,31 @@
-
 import { Suspense } from 'react'
 import ToggleDB from './components/filters/inputs/toggle-db'
 import SearchBar from './components/filters/inputs/search-bar'
 import SkeletonStudentsTable from './components/skeletons/skeleton-students-table'
 import StudentsTable from './components/students-table'
 
-export interface SearchParams {
-  anio?: string
-  search?: string
-  cursos?: string
-  materias?: string
-  califParciales?: string
-  inclusionEstricta?: string
-  cantidadesTroncales?: string
-  cantidadesGenerales?: string
-  cantidadesEnProceso2020?: string
-  enProceso2020?: string
-  cantOptativo?: string
-  repitenciaAnios?: string
-  repitenciaCant?: string
-  proyeccion?: string
-  page?: string
-  sort?: string
+const searchParamsKeys = [
+  'anio',
+  'search',
+  'cursos',
+  'materias',
+  'califParciales',
+  'inclusionEstricta',
+  'cantidadesTroncales',
+  'cantidadesGenerales',
+  'cantidadesEnProceso2020',
+  'enProceso2020',
+  'cantOptativo',
+  'repitenciaAnios',
+  'repitenciaCant',
+  'proyeccion',
+  'page',
+  'sort',
+] as const
+
+export type SearchParams = {
+  // eslint-disable-next-line no-unused-vars
+  [key in typeof searchParamsKeys[number]]?: string
 }
 
 export default async function Page({
@@ -30,6 +34,8 @@ export default async function Page({
   searchParams: Promise<SearchParams>
 }) {
   const awaitedSearchParams = await searchParams
+  const filterValues = getFiltersValues(awaitedSearchParams)
+  console.log(filterValues)
   return (
     <div className="grid w-full grid-rows-[auto_auto_auto] gap-x-6 gap-y-3 px-0 lg:grid-cols-[minmax(230px,1fr)_7fr] lg:px-4 xl:gap-x-8 xl:gap-y-4 2xl:px-8">
       <ToggleDB className="hidden lg:block" />
@@ -43,3 +49,30 @@ export default async function Page({
     </div>
   )
 }
+
+type FiltersValues = Partial<
+  Record<keyof SearchParams, string | string[] | number[]>
+> /* {
+  [key in keyof SearchParams]: string | string[] | number[]
+} */
+
+function getFiltersValues(searchParams: object): FiltersValues {
+  const filtersValues: FiltersValues = {}
+  Object.keys(searchParams).forEach((key) => {
+    if (searchParamsKeys.includes(key)) {
+      const param = searchParams[key]
+      if (param !== undefined) {
+        filtersValues[key] = param
+      }
+    }
+  })
+  return filtersValues
+}
+
+const isValidKey = <T extends object>(
+  key: string,
+  object: T,
+): key is Extract<keyof T, string> => key in object
+const isKeyOfType = <T extends object>(
+  key: string,
+): key is Extract<keyof T, string> => key in ({} as T)
